0827bd8727ac0738fb6d6270243bb7175d9409a8
Faizul Osman <faizulosmanrabby@gmail.com>
2022-10-11 12:20:24 +0600

    blog page done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,78 @@
 import React from "react";
 
 const Blog = () => {
-  return <div></div>;
+  return (
+    <div className="w-11/12 mx-auto my-24">
+      <h2 className="text-center mb-10 text-xl md:text-4xl font-semibold">
+        Questions And Answers
+      </h2>
+      <div className="w-11/12 mx-auto">
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border bg-base-100 rounded-md"
+        >
+          <div className="collapse-title text-xl font-medium">
+            What is the purpose of react router?
+          </div>
+          <div className="collapse-content">
+            <p>
+              ReactJS Router is mainly used for developing Single Page Web
+              Applications. React Router is used to define multiple routes in
+              the application. React Router is a standard library for routing in
+              React. It enables the navigation among views of various components
+              in a React Application, allows changing the browser URL, and keeps
+              the UI in sync with the URL.
+            </p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border bg-base-100 rounded-md"
+        >
+          <div className="collapse-title text-xl font-medium">
+            How does Context API works?
+          </div>
+          <div className="collapse-content">
+            <p>
+              In a typical React application, data is passed top-down (parent to
+              child) via props, but such usage can be cumbersome for certain
+              types of props (e.g. locale preference, UI theme) that are
+              required by many components within an application. Context
+              provides a way to share values like these between components
+              without having to explicitly pass a prop through every level of
+              the tree.
+            </p>
+          </div>
+        </div>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border bg-base-100 rounded-md"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Do you know about useRef hook? Tell me something about it.
+          </div>
+          <div className="collapse-content">
+            <p>
+              useRef returns a mutable ref object whose .current property is
+              initialized to the passed argument ( initialValue ). The returned
+              object will persist for the full lifetime of the component.
+              Essentially, useRef is like a “box” that can hold a mutable value
+              in its .current property.
+              <li>
+                The useRef Hook allows you to persist values between renders. It
+                can be used to access a DOM element directly.
+              </li>
+              <li>
+                It can be used to store a mutable value that does not cause a
+                re-render when updated.
+              </li>
+              <li>It can be used to access a DOM element directly.</li>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- src/components/SingleQuiz.jsx
+++ src/components/SingleQuiz.jsx
@@ -1,5 +1,7 @@
+import { faCircleDot, faEye } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { div, NavLink } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -14,42 +16,101 @@ const SingleQuiz = ({ quiz }) => {
     }
   };
 
+  const showCorrectAnwer = (correctAnswer) => {
+    toast(`Correct Answer: ${correctAnswer}`);
+  };
+
   return (
     <div className="">
       <div className="card bg-base-100 border shadow-xl">
         <div className="card-body">
-          <h2 className="card-title text-center mb-4">Quiz : {question}</h2>
-          <div className="grid grid-cols-2 gap-2">
-            <NavLink
-              onClick={() => handleSelectedAnswer(options[0])}
-              style={({ isActive }) =>
-                isActive ? { backgroundColor: "aqua" } : undefined
-              }
-              className="btn btn-outline btn-primary h-full py-4"
+          <div className="flex justify-between">
+            <h2 className="card-title text-center mb-4">Quiz : {question}</h2>
+            <button
+              onClick={() => showCorrectAnwer(correctAnswer)}
+              className="btn bg-base-100 border-none"
             >
+              <FontAwesomeIcon icon={faEye} />
+            </button>
+          </div>
+          <div className="grid grid-cols-2 gap-2">
+            <div
+              onClick={() => handleSelectedAnswer(options[0])}
+              className="border border-primary rounded-md flex items-center h-full py-4"
+            >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500 mx-1 sm:mx-5"
+              />
               {options[0]}
-            </NavLink>
-            <NavLink
-              style={({ isActive }) =>
-                !isActive ? { backgroundColor: "aqua" } : undefined
-              }
+            </div>
+            <div
+              onClick={() => handleSelectedAnswer(options[1])}
+              className="border border-primary rounded-md flex items-center h-full py-4"
+            >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500 mx-5"
+              />
+              {options[1]}
+            </div>
+            <div
+              onClick={() => handleSelectedAnswer(options[2])}
+              className="border border-primary rounded-md flex items-center h-full py-4"
+            >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500 mx-5"
+              />
+              {options[2]}
+            </div>
+            <div
+              onClick={() => handleSelectedAnswer(options[3])}
+              className="border border-primary rounded-md flex items-center h-full py-4"
+            >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500 mx-5"
+              />
+              {options[3]}
+            </div>
+            {/* <div
               onClick={() => handleSelectedAnswer(options[1])}
               className="btn btn-outline btn-primary h-full py-4"
             >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500"
+              />
               {options[1]}
-            </NavLink>
-            <NavLink
+            </div>
+            <div
               onClick={() => handleSelectedAnswer(options[2])}
               className="btn btn-outline btn-primary h-full py-4"
             >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500"
+              />
               {options[2]}
-            </NavLink>
-            <NavLink
+            </div>
+            <div
               onClick={() => handleSelectedAnswer(options[3])}
               className="btn btn-outline btn-primary h-full py-4"
             >
+              <input
+                type="radio"
+                name="radio-6"
+                className="radio checked:bg-blue-500"
+              />
               {options[3]}
-            </NavLink>
+            </div> */}
           </div>
         </div>
       </div>
